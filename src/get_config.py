abc5dd8364161d75448ac983e708032f492d1189
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-11-27 22:52:00 +0300

    feat(get_config): Implement basic config functionality

--- src/get_config.py
+++ src/get_config.py
@@ -1,15 +1,31 @@
 import yaml
 from internal import eprint
-from os import environ
+from os import environ, path, makedirs
 
+config_home = ""
 config_path = ""
 
-if environ.get("HOME") is not None:
-    config_path = environ.get("HOME") + "/.config/syncgdrive/config.yaml"
+# Check if config home exist
+if environ.get("HOME") is not None and environ.get("XDG_CONFIG_HOME") is not None:
+    config_home = environ.get("XDG_CONFIG_HOME")
+elif environ.get("HOME") is not None:
+    config_home = environ.get("HOME") + "/.config"
 else:
-    eprint("User doesn't have a home directory", "ERR")
+    eprint("User doesn't have a config directory.", "ERR")
 
-if environ.get("XDG_CONFIG_HOME") is not None:
-    config_path = environ.get("XDG_CONFIG_HOME")
+# Create the config path if doesn't exist
+if config_home != "":
+    config_path = config_home + "/syncgdrive/config.yaml"
+    if not path.exists(config_path):
+        makedirs(path.dirname(config_path), exist_ok=True)
+        with open(config_path, "a") as f:
+            pass
+else:
+    eprint("User doesn't have a config directory.", "ERR")
 
-print(config_path)
+with open(config_path, "r") as file:
+    config = yaml.safe_load(file)
+
+
+def get_trusted_networks():
+    return config["trusted_networks"]
