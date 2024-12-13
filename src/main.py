34a9c8a878d8a52be22e844bd1990b88730187e5
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-12-14 02:01:58 +0300

    feat(main): warn for the empty lists and exit if both of the lists are empty

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ def process_rclone_array(array, list_name="the"):
     home_expanded_array = []
     processed_array = []
     non_existent_paths = []
+    is_empty = False
 
     if array is not None:
         for item in array:
@@ -41,9 +42,14 @@ def process_rclone_array(array, list_name="the"):
         else:
             non_existent_paths.append(item)
 
+    if not processed_array:
+        is_empty = True
+        eprint(f"{list_name.capitalize()} list is empty", "WARN")
+
     return {
         "processed_array": processed_array,
         "non_existent_paths": non_existent_paths,
+        "is_empty": is_empty,
     }
 
 
@@ -77,6 +83,7 @@ def main():
     # Warn the user if there are invalid paths
     if non_existent_paths:
         non_existent_string = "\n".join(non_existent_paths)
+        print(end="\n")
         eprint(f"Below paths doesn't exist:", "WARN")
         print(non_existent_string)
         print(end="\n")
@@ -86,6 +93,9 @@ def main():
             "folder-sync",
         )
         non_existent_notify.show()
+
+    if backup_list["is_empty"] and sync_list["is_empty"]:
+        eprint("No backup or sync item found", "ERR")
 
     asyncio.run(
         backup_and_sync_concurrently(
